63da702de84e08ce5b8b8e92f03d4f9f29db06f8
Ayan Dave <daveayan@gmail.com>
2013-05-09 04:13:39 -0400

    javvadoc

--- src/main/java/com/daveayan/transformers/CanTransform.java
+++ src/main/java/com/daveayan/transformers/CanTransform.java
@@ -1,5 +1,10 @@
 package com.daveayan.transformers;
 
+/**
+ * A common interface
+ * @author adave
+ *
+ */
 public interface CanTransform {
 	public boolean canTransform(Object from, Class<?> to, Context context);
 	public Object transform(Object from, Class<?> to, Context context) ;
